f6e6d99aae62d558dbfd78555d4c26b7eba8974e
Rodion <pensioneer4ik@gmail.com>
2016-07-11 02:27:12 +0300

    commit

--- Trial/js/common.js
+++ Trial/js/common.js
@@ -101,3 +101,21 @@ theToggle.onclick = function() {
    toggleClass(this, 'on');
    return false;
 }
+
+
+
+
+$(document).ready(function() {
+  $('.tabs li').click(function(){
+    if ($(this).hasClass('selected')===false) {
+      $('.tabs li').removeClass('selected');
+      $(this).addClass('selected');
+    }
+    var selectionId = $(this).attr('id');
+    $('.content').fadeOut('fast', function(){
+      $('div .page').css('display','none');
+      $('.page#'+selectionId).css('display','block');
+      $('.content').fadeIn('fast');
+    });
+  });
+});
